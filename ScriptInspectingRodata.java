c4222a1292d95317784f0899e7d037caa4de19cd
ClementB-etu <belleil.clement00@gmail.com>
2022-06-01 17:11:59 +0200

    commit (inspecting .rodata)

--- ScriptInspectingRodata.java
+++ ScriptInspectingRodata.java
@@ -21,27 +21,26 @@
 
 import ghidra.app.script.GhidraScript;
 
-import ghidra.program.model.address.Address;
-import ghidra.program.model.mem.Memory;
-import ghidra.program.model.mem.MemBuffer;
-import ghidra.program.model.listing.Listing;
-import ghidra.program.model.listing.Instruction;
-import ghidra.program.model.listing.InstructionIterator;
-import ghidra.program.model.listing.Function;
-import ghidra.program.model.listing.FunctionManager;
-import ghidra.program.model.listing.FunctionIterator;
-import ghidra.program.model.listing.CodeUnit;
+import generic.continues.RethrowContinuesFactory;
+import ghidra.app.util.bin.ByteProvider;
+import ghidra.app.util.bin.MemoryByteProvider;
 
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressSpace;
+import ghidra.program.model.address.AddressSet;
+import ghidra.program.model.mem.*;
+import ghidra.program.model.mem.MemoryBlock;
+import ghidra.program.model.mem.MemBuffer;
+import ghidra.program.model.mem.MemoryAccessException;
+import ghidra.program.model.listing.*;
 
 import ghidra.util.Msg;
 import java.lang.Math;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;  
+import java.util.Map.Entry;
 import java.io.*;
-import java.util.Scanner;
 import java.nio.file.*;
-import java.util.Arrays;
+import java.nio.charset.Charset;
 
 /*
  * TODO
@@ -52,7 +51,7 @@ import java.util.Arrays;
  *
 */
 
-public class Scriptv1 extends GhidraScript {
+public class ScriptInspectingRodata extends GhidraScript {
 
     @Override
     protected void run() throws Exception {
@@ -71,13 +70,8 @@ public class Scriptv1 extends GhidraScript {
         ByteProvider byteProvider = new MemoryByteProvider(mem, currentProgram.getImageBase());
         MemoryBlock[] memblocksSections = mem.getBlocks();
 
-        
-        SymbolTable symtab = currentProgram.getSymbolTable();
-        SymbolIterator symit = symtab.getAllSymbols(false);
-        
-        LinkedHashMap<String, Integer> dataAddr = new LinkedHashMap<String, Integer>();
-        Map<Address, String> dataInfo = new HashMap<Address, String>();
 
+        /*
         while (symit.hasNext())
         {   
             Symbol sym = symit.next();
@@ -101,40 +95,57 @@ public class Scriptv1 extends GhidraScript {
                 }
             }            
         }
-
+        */
         for (MemoryBlock secblock : memblocksSections) {
-            if (secblock.getName().equals(".text"))
+            if (secblock.getName().equals(".rodata"))
             {
-                pw.println(section  + secblock.getName());        
-                pw.println("_start :");   
-                while ((listIt.hasNext())) {
-                    Instruction instr = listIt.next();
-                    pw.println("\t\t" + instr.toString().replace("ptr",""));
-                }
-     
-            } 
-            else if ((secblock.getName().equals(".data"))) 
-            {
-                pw.println(section  + secblock.getName());   
-                byte[] b = new byte[(int)secblock.getSize()];
+                /*byte[] b = new byte[(int)secblock.getSize()];
                 if (secblock.getBytes(secblock.getStart(),b) > -1)
                 {
-                    println("[ "+ secblock.getName() +" ] : bytes retreived\n");
+                    println("[ "+ secblock.getName() +" ] : bytes retreived");
                 }
+                int indtmp = 0;
+                for (int i = 0;i<b.length;i++) {
+                    if (b[i]==0) 
+                    {
+                        printf("\nNew string : ");
+                        byte[] btmp = getSliceOfArray(b,indtmp,i);
+
+                        for (int j = 0;j<btmp.length;j++) printf(String.format("%02x", btmp[j]));
+                        //String string = new String(btmp);
+                        //println(string);
+                        if
+                        indtmp = i;
+                    }
+                }
+                int cpt = 0;
+                */
+
+                /* TODO : imports */ 
+
+                AddressSpace aSpace = currentProgram.getAddressFactory().getDefaultAddressSpace();
+                ElfCompatibilityProvider elfCompatProvider = new ElfCompatibilityProvider(currentProgram, false);
+                Address thisAddr = aSpace.getAddress(mem.getLong(rttiBaseAddr.add(0x8)));
+                String symbol = elfProvider.getReader().readTerminatedString(thisAddr.getOffset(), '\0');
                 
-                int start = 0;
-                int end = 0;
-                for (Map.Entry<String, Integer> entry : dataAddr.entrySet())
-                {   
-                    println(entry.getKey() + " start : " + start + " end : " + end);
-                    end = entry.getValue()-1;
-                    byte[] btmp = Arrays.copyOfRange(b,start,start+end);
-                    start = entry.getValue();
-
-                    String string = new String(btmp);
-                    pw.println( entry.getKey() +" : db\t'" + string + "',10");
-                }              
-
+            } else {
+                //println("Section : " + secblock.getName());        
             }
         }
+    }
+
+    public static byte[] getSliceOfArray(byte[] arr, int start, int end)
+    {
+  
+        // Get the slice of the Array
+        byte[] slice = new byte[end - start];
+  
+        // Copy elements of arr to slice
+        for (int i = 0; i < slice.length; i++) {
+            slice[i] = arr[start + i];
+        }
+  
+        // return the slice
+        return slice;
+    }
 }
